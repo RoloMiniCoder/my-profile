1d2aa002c4b442e8eafd9ae97eecf82490521864
Diogo Rolo <diogo.rolo@codeforall.com>
2025-11-20 12:46:19 +0000

    Add some more touches to music page

--- src/pages/Music/index.jsx
+++ src/pages/Music/index.jsx
@@ -2,24 +2,30 @@ import TopList from './TopList';
 import WeeklySummary from './WeeklySummary';
 
 const profileList = [
-  { link: 'https://www.last.fm/user/darkstahrl', name: 'Last.fm' },
-  { link: 'https://tidal.com/artist/63737051', name: 'Tidal' },
-  { link: 'https://open.spotify.com/user/1273789846?si=ed0887ea370a4d08', name: 'Spotify' },
+  { link: 'https://www.last.fm/user/darkstahrl', name: 'Last.fm', img: `/icons8-last.fm-96.png` },
+  { link: 'https://tidal.com/artist/63737051', name: 'Tidal', img: `/icons8-spotify-96.png` },
+  { link: 'https://open.spotify.com/user/1273789846?si=ed0887ea370a4d08', name: 'Spotify', img: `public/icons8-tidal-96.png` },
 ];
 
 function ProfileLinks({ profiles }) {
   return (
     <section id='music-profile-links'>
       <h2>Find the stuff I listen to on:</h2>
-      <ul>
+      <div>
         {profiles.map((profile) => (
-          <li key={profile.link}>
-            <a href={profile.link} target="_blank" rel="noopener noreferrer">
-              {profile.name}
-            </a>
-          </li>
+          <a key={profile.link} href={profile.link} target="_blank" rel="noopener noreferrer">
+            <div>
+              <img src={profile.img} alt={`${profile.name} logo`} />
+              <p>
+                {profile.name}
+              </p>
+            </div>
+          </a>
         ))}
-      </ul>
+      </div>
+      <p>
+        <small>
+          <a target="_blank" href="https://icons8.com/icon/c1Sd6GHCDlb9/tidal">Tidal</a> <a target="_blank" href="https://icons8.com/icon/zF4qMoedBHZT/lastfm">Last.fm</a> and <a target="_blank" href="https://icons8.com/icon/G9XXzb9XaEKX/spotify">Spotify</a> icons by <a target="_blank" href="https://icons8.com">Icons8</a></small></p>
     </section>
   );
 }
